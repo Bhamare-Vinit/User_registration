7f1a4ca0f2ee8d956b134123390a8897d82dae04
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-15 14:29:58 +0530

    [vinit] add: check valid password rule3

--- main.py
+++ main.py
@@ -20,24 +20,24 @@ def check_if_valid_number(number):
 
 def check_if_valid_password(password):
     if  len(password)>=8:
-        if re.search(r"[A-Z]",password):
+        if re.search(r"[A-Z]",password) and re.search(r"\d",password):
             return True
     else:
         return False
 
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-email = input("Enter your email: ")
-number=input("Enter your mobile number with country code: ")
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# email = input("Enter your email: ")
+# number=input("Enter your mobile number with country code: ")
 password=input("Enter your password: ")
 
-if(check_if_valid_firstname(first_name,last_name) and check_if_valid_email(email) and check_if_valid_number(number)
-    and check_if_valid_password(password)):
-    print("Valid user input")
-else:
-    print("Invalid user input")
-
-# if( check_if_valid_password(password)):
+# if(check_if_valid_firstname(first_name,last_name) and check_if_valid_email(email) and check_if_valid_number(number)
+#     and check_if_valid_password(password)):
 #     print("Valid user input")
 # else:
 #     print("Invalid user input")
+
+if( check_if_valid_password(password)):
+    print("Valid user input")
+else:
+    print("Invalid user input")
