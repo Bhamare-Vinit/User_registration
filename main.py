93cd781273bdddf305958e146f9b2ee45b086d22
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-15 13:55:51 +0530

    [vinit] add: check valid user mobile number

--- main.py
+++ main.py
@@ -18,13 +18,19 @@ def check_if_valid_email(email):
         return True
     else:
         return False
-
+    
+def check_if_valid_number(number):
+    if re.search(r"^\d{2}\s\d{10}$", number):
+        return True
+    else:
+        return False
 
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 email = input("Enter your email: ")
+number=input("Enter your mobile number with country code: ")
 
-if(check_if_valid_firstname(first_name,last_name) and check_if_valid_email(email)):
+if(check_if_valid_firstname(first_name,last_name) and check_if_valid_email(email) and check_if_valid_number(number)):
     print("Valid user input")
 else:
     print("Invalid user input")
